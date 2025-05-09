f418fc5178d690bb677bdba5a5387d270aa55247
songhourchhuy <songhourchhuy@gmail.com>
2025-05-09 16:53:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let cart = [];
 const flavors = [
-    { name: 'Vanilla', price: 1.90, category: 'normal', image: 'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Vanilla.png?raw=true', icon:  'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Vanilla.png?raw=true' }, 
-    { name: 'Chocolate', price: 1.90, category: 'normal', image: 'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Chocolate.png?raw=true', icon:  'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Chocolate.png?raw=true' },  },
-    { name: 'Pistachio', price: 2.10, category: 'special', image: 'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Pistachio.png?raw=true', icon:  'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Pistachio.png?raw=true' },  },
-    { name: 'Mango', price: 2.10, category: 'special', image: 'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Mango.png?raw=true', icon:  'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Mango.png?raw=true' },  },
-    { name: 'Ice Cream Pack', price: 5.00, category: 'takeaway', image: 'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Durian.png?raw=true', icon:  'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Durian.png?raw=true' },  },
-    { name: 'Chocolate Chips', price: 0.50, category: 'add-ons', image: 'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Hazelnut.png?raw=true', icon:  'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Hazelnut.png?raw=true' },  },
-    { name: 'Fruit Toppings', price: 0.80, category: 'add-ons', image: 'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Yogurt.png?raw=true', icon:  'https://github.com/songhourchhuy/gelato-shop/blob/main/Menu%20image/Yogurt.png?raw=true' },  }
+    { name: 'Vanilla', price: 1.90, category: 'normal', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Vanilla.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Vanilla.png' }, 
+    { name: 'Chocolate', price: 1.90, category: 'normal', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Chocolate.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Chocolate.png' },
+    { name: 'Pistachio', price: 2.10, category: 'special', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Pistachio.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Pistachio.png' }, 
+    { name: 'Mango', price: 2.10, category: 'special', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Mango.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Mango.png' }, 
+    { name: 'Ice Cream Pack', price: 5.00, category: 'takeaway', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Durian.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Durian.png' }, 
+    { name: 'Chocolate Chips', price: 0.50, category: 'add-ons', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Hazelnut.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Hazelnut.png' }, 
+    { name: 'Fruit Toppings', price: 0.80, category: 'add-ons', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Yogurt.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Yogurt.png' }
 ];
 
 // Function to render the categories
