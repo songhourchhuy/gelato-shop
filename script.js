6b33e2ab77afd49b2be0937f0bee582accc03404
songhourchhuy <songhourchhuy@gmail.com>
2025-05-09 17:08:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let cart = [];
 const flavors = [
-    { name: 'Vanilla', price: 1.90, category: 'normal', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Vanilla.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Vanilla.png' },
-    { name: 'Chocolate', price: 1.90, category: 'normal', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Chocolate.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Chocolate.png' },
-    { name: 'Pistachio', price: 2.10, category: 'special', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Pistachio.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Pistachio.png' },
-    { name: 'Mango', price: 2.10, category: 'special', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Mango.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Mango.png' },
-    { name: 'Ice Cream Pack', price: 5.00, category: 'takeaway', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Durian.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Durian.png' },
-    { name: 'Chocolate Chips', price: 0.50, category: 'add-ons', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Hazelnut.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Hazelnut.png' },
-    { name: 'Fruit Toppings', price: 0.80, category: 'add-ons', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Yogurt.png', icon:  'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Yogurt.png' }
+    { name: 'Vanilla', price: 1.90, category: 'normal', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Vanilla.png' },
+    { name: 'Chocolate', price: 1.90, category: 'normal', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Chocolate.png' },
+    { name: 'Pistachio', price: 2.10, category: 'special', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Pistachio.png' },
+    { name: 'Mango', price: 2.10, category: 'special', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Mango.png' },
+    { name: 'Ice Cream Pack', price: 5.00, category: 'takeaway', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Durian.png' },
+    { name: 'Chocolate Chips', price: 0.50, category: 'add-ons', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Hazelnut.png' },
+    { name: 'Fruit Toppings', price: 0.80, category: 'add-ons', image: 'https://raw.githubusercontent.com/songhourchhuy/gelato-shop/main/Menu%20image/Yogurt.png' }
 ];
 
 // Function to render the categories
@@ -22,7 +22,6 @@ function displayCategories() {
             flavorDiv.innerHTML = `
                 <img src="${flavor.image}" alt="${flavor.name}">
                 <h3>${flavor.name} - $${flavor.price}</h3>
-                <img src="${flavor.icon}" alt="${flavor.name} icon" class="item-icon">
                 <button onclick="addToCart('${flavor.name}', ${flavor.price})">Add to Cart</button>
             `;
             categoryDiv.appendChild(flavorDiv);
@@ -46,7 +45,6 @@ function filterFlavors() {
         flavorDiv.innerHTML = `
             <img src="${flavor.image}" alt="${flavor.name}">
             <h3>${flavor.name} - $${flavor.price}</h3>
-            <img src="${flavor.icon}" alt="${flavor.name} icon" class="item-icon">
             <button onclick="addToCart('${flavor.name}', ${flavor.price})">Add to Cart</button>
         `;
         categoryDiv.appendChild(flavorDiv);
